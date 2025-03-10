651e36ee843dd8235c6199bb857e087267d215e8
Geυмjυ lee <97003348+GoldenPearls@users.noreply.github.com>
2025-03-10 22:30:07 +0900

    [미션1] 깃허브 리포지토리에 프로젝트 올리기

---
+++ src/main/kotlin/com/geumjulee/meow_diary/MeowDiaryApplication.kt
@@ -0,0 +1,11 @@
+package com.geumjulee.meow_diary
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MeowDiaryApplication
+
+fun main(args: Array<String>) {
+	runApplication<MeowDiaryApplication>(*args)
+}
---
+++ src/test/kotlin/com/geumjulee/meow_diary/MeowDiaryApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.geumjulee.meow_diary
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MeowDiaryApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
